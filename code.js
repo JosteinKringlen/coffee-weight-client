d4b30f1fc7242788fec5bdbaf8101af6df710092
Jostein Kringlen <josteinkringlen@gmail.com>
2018-05-31 14:34:00 +0200

    Initial commit

---
+++ code.js
@@ -0,0 +1,54 @@
+const $ = require('jquery');
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort('/dev/cu.usbmodem14221', {autoOpen: true, baudRate: 9600});
+const qs = require('qs');
+const credentials = require('./credentials');
+
+// Update time in seconds
+const updateTime = 30;
+
+$(document).ready(function () {
+    runCodeContinuously();
+});
+
+function runCodeContinuously(){
+    $("button").click(() => {
+        port.write("tare", function (err) {
+            if(err){
+                return console.log(err)
+            }
+        });
+        console.log("good stuff");
+    });
+
+    // Removes the "flow" mode of the serial input,
+    // and gives each number on a single line
+    let parser = new Readline();
+    let counter = 0;
+    port.pipe(parser);
+    parser.on('data', function (data) {
+        $("#kg").text(data.toString());
+        counter++;
+        if(counter === updateTime){
+            updateWeightText(data);
+            counter = 0;
+        }
+    });
+}
+
+function updateWeightText(weight) {
+    console.log(weight.toString());
+    //$("#kg").text(weight.toString());
+    /*fetch('https://api.thingspeak.com/update.json?',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: qs.stringify({
+            api_key: credentials.api_key,
+            field1: weight.toString()
+        })
+    })
+        .catch(err => console.log(err))*/
+}
