91f9244fdd0ba35a99c217daf454d3d8a93eeb61
Jostein Kringlen <josteinkringlen@gmail.com>
2018-06-01 12:22:41 +0200

    Changed electron to be full screen

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let win
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 480, height: 800})
+    win = new BrowserWindow({width: 480, height: 800, fullscreen: true})
 
     // and load the index.html of the app.
     win.loadFile('index.html')
