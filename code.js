d1ff9fc564095c320da2c9084e355685ad1ba7df
Jostein Kringlen <josteinkringlen@gmail.com>
2018-06-01 17:45:28 +0200

    Small update to make the tare button work again

--- code.js
+++ code.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 });
 
 function runCodeContinuously(){
-    $("button").click(() => {
+    $("#button").click(() => {
         port.write("tare", function (err) {
             if(err){
                 return console.log(err)
